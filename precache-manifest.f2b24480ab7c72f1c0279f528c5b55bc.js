4b4d4eab3fe34ee1c4586e548d07645cfb50c3d5
SneX <icegirl-13@hotmail.com>
2019-08-09 01:35:32 +0200

    Updates

---
+++ precache-manifest.f2b24480ab7c72f1c0279f528c5b55bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2492231ccc4a8d331475f0a483a4d98",
+    "url": "/react-bootcamp/index.html"
+  },
+  {
+    "revision": "5d8e71dcf640d1fc099e",
+    "url": "/react-bootcamp/static/css/main.910b3802.chunk.css"
+  },
+  {
+    "revision": "f8f35da122a36a9fe6c6",
+    "url": "/react-bootcamp/static/js/2.7dd47fc0.chunk.js"
+  },
+  {
+    "revision": "5d8e71dcf640d1fc099e",
+    "url": "/react-bootcamp/static/js/main.7cc8fc8f.chunk.js"
+  },
+  {
+    "revision": "ff8ef94d70a40669f403",
+    "url": "/react-bootcamp/static/js/runtime~main.52b29217.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-bootcamp/static/media/logo.5d5d9eef.svg"
+  }
+]);
